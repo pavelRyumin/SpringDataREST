af4d0dd84cabe525b5053e39b0dbe195d6577416
Paul-VS-prog <74456837+Paul-VS-prog@users.noreply.github.com>
2022-06-24 17:05:17 +0300

    Initial commit

---
+++ src/test/java/com/ryumin/spring/springboot/springdatarest/SpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryumin.spring.springboot.springdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
